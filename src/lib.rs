2597676a507e0b49efb2e60df468ba40304c3e3c
benjaminwgordon <benjaminwillardgordon@gmail.com>
2023-05-01 17:09:55 -0500

    rebuild for 3d geometry instead of lines

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+mod utils;
+
+use hilbert_curve_generator::HilbertCurve;
+use js_sys;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[macro_export]
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into())
+    }
+}
+
+#[wasm_bindgen]
+pub fn hilbert_coordinates(n: u32, p: u32) -> js_sys::Uint32Array {
+    let hilbert_curve = HilbertCurve::new(n, p).unwrap();
+    log!("{hilbert_curve:?}");
+    let out = js_sys::Uint32Array::new_with_length((hilbert_curve.coordinates.len() * 3) as u32);
+
+    for i in 0..hilbert_curve.coordinates.len() {
+        let (x, y, z) = hilbert_curve.coordinates[i];
+        out.set_index((i * 3) as u32, x);
+        out.set_index((i * 3 + 1) as u32, y);
+        out.set_index((i * 3 + 2) as u32, z);
+    }
+
+    out
+}
