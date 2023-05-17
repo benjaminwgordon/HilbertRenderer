20acc252a0bf7ef36ec178418d646c52980e377d
benjaminwgordon <benjaminwillardgordon@gmail.com>
2023-05-17 16:23:58 -0500

    adds title and favicon

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -14,6 +14,8 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>Ben Gordon</title>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
